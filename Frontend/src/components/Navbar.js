23874d476e8ffe79ef11d539f08b3915ed1ab067
Maylo <camilo.iegamar.z@gmail.com>
2020-09-29 12:24:23 -0500

    Primeras pantallas React, 0.1

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { GrSolaris } from 'react-icons/gr';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <nav className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <GrSolaris className='navbar-icon' />
+              BioSolar
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Inicio
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Servicios
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/products'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Producto
+                </Link>
+              </li>
+              <li className='nav-btn'>
+                {button ? (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button buttonStyle='btn--outline'>REGÍSTRATE</Button>
+                  </Link>
+                ) : (
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button
+                      buttonStyle='btn--outline'
+                      buttonSize='btn--mobile'
+                      onClick={closeMobileMenu}
+                    >
+                      SIGN UP
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ Frontend/src/components/pages/Footer.js/Footer.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from '../../Button';
+import { Link } from 'react-router-dom';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { GrSolaris } from 'react-icons/gr';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+        Suscribete para recibir las últimas noticias de las actualizaciones y cambios en BioSolar
+        </p>
+        <p className='footer-subscription-text'>
+        Puede anular la suscripción en cualquier momento. 
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Tu correo electrónico'
+            />
+            <Button buttonStyle='btn--outline'>Suscribirse</Button>
+          </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Sobre nosotros</h2>
+            <Link to='/sign-up'>Como funciona</Link>
+            <Link to='/'>Testimonios</Link>
+            <Link to='/'>Aliados </Link>
+            <Link to='/'>Políticas de privacidad</Link>
+            <Link to='/'>Términos de servicio </Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Contáctenos</h2>
+            <Link to='/'>Contacto</Link>
+            <Link to='/'>Soporte </Link>
+            <Link to='/'>Donar</Link>
+            <Link to='/'>Patrocinar</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Comunidad</h2>
+            <Link to='/'>Colaborar</Link>
+            <Link to='/'>Github</Link>
+            <Link to='/'>P.C.J.I.C</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Redes Sociales</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              <GrSolaris className='navbar-icon' />
+              BioSolar
+            </Link>
+          </div>
+          <small className='website-rights'>BioSolar © 2020</small>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to={
+                ''
+              }
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <FaTwitter />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
